5e21242766baf209d12831fa641311c9dab95ed5
lanhhoang <lanh.hoangcong@gmail.com>
2016-12-14 20:58:04 +0700

    Add Navbar

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   def self.search(search)
     where("title ILIKE ?", "%#{search}%") 
-    where("body ILIKE ?", "%#{search}%")
+    # where("body ILIKE ?", "%#{search}%")
   end
 end
