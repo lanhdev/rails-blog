2c47e30fa5085fac87440f53e267fe68f9f7c704
lanhhoang <lanh.hoangcong@gmail.com>
2016-12-18 23:19:48 +0700

    Add view number for an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  impressionist :actions=>[:show]
 
   # GET /articles
   # GET /articles.json
@@ -28,7 +29,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.html { redirect_to articles_url, notice: 'Article was successfully created.' }
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new }
@@ -42,7 +43,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+        format.html { redirect_to articles_url, notice: 'Article was successfully updated.' }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit }
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
+  is_impressionable
   def self.search(search)
-    where("title ILIKE ?", "%#{search}%") 
+    where("title || body ILIKE ?", "%#{search}%") 
     # where("body ILIKE ?", "%#{search}%")
   end
 end
