e6c87c29af1d6a7b4d363cfa75ed32aaf9bd67b4
lanhhoang <lanh.hoangcong@gmail.com>
2016-12-20 01:30:35 +0700

    Add comment function

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true, length: { minimum: 5 }
   is_impressionable
   def self.search(search)
     where("title || body ILIKE ?", "%#{search}%") 
