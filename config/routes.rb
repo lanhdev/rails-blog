dc74b945fb129266ce29fa3d0110cc0bc05466b0
lanhhoang <lanh.hoangcong@gmail.com>
2016-12-24 19:09:33 +0700

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :articles do
     resources :comments
   end
   get 'tags/:tag', to: 'articles#index', as: "tag"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'articles#index'
+  root 'welcome#index'
 end
